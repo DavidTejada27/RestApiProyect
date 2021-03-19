19e231876a32c9c5c9d8e95f8f1cb11851b24eea
David <david.tejada27g@gmail.com>
2021-03-19 01:43:01 -0400

    Started Api

---
+++ RestApiProyect/Model/NarrativeInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiProyect.Model
+{
+    public class Entry
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Organization
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Href { get; set; }
+    }
+
+    public class NarrativeInformation
+    {
+        public string Id { get; set; }
+        public string Source { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string DateTime { get; set; }
+        public string Author { get; set; }
+        public IList<Entry> Entries { get; set; }
+        public Organization Organization { get; set; }
+    }
+}
---
+++ RestApiProyect/Services/INarrativeApiService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiProyect.Services
+{
+    public interface INarrativeApiService
+    {
+    }
+}
